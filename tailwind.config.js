4bc46006035266e2fdd09fd3a61a5c104c04d451
rahatulahsan <rahatulahsan@gmail.com>
2023-08-12 00:27:29 +0600

    Dark Mode Completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,10 @@ module.exports = {
     extend: {
       colors:{
         fylodark: '#1c2431',
+        fylodarksection: '#181f2a',
+        fylodarkreview: '#202a3c',
         fylocyan: '#62e0d9',
+        fylofooter: '#0b1523',
       }
     },
   },
